caf301fb287ab211ec6291ce5c37a2d52beb6afd
Aryan <ariyasaran200@gmail.com>
2024-08-03 18:36:53 +0530

    the main slam and view

---
+++ Monocular/slam.py
@@ -0,0 +1,71 @@
+import cv2
+from view import Display
+from extractor import Frame, denormalize, match_frames, IRt, add_ones
+import numpy as np
+from pointmap import Map, Point
+ 
+W, H = 1920//2, 1080//2
+ 
+F = 270
+ 
+K = np.array(([F, 0, W//2], [0,F,H//2], [0, 0, 1]))
+Kinv = np.linalg.inv(K)
+ 
+display = Display(W, H)
+
+mapp = Map()
+mapp.create_viewer()
+
+	
+def process_frame(img):
+    img = cv2.resize(img, (W, H))
+    frame = Frame(mapp, img, K)
+    if frame.id == 0:
+        return
+ 
+
+    f1 = mapp.frames[-1]
+    f2 = mapp.frames[-2]
+ 
+    idx1, idx2, Rt = match_frames(f1, f2)
+     
+
+
+    f1.pose = np.dot(Rt, f2.pose)
+
+    pts4d = triangulate(f1.pose, f2.pose, f1.pts[idx1], f2.pts[idx2])
+
+    pts4d /= pts4d[:, 3:]
+ 
+    good_pts4d = (np.abs(pts4d[:, 3]) > 0.005) & (pts4d[:, 2] > 0)
+ 
+    for i, p in enumerate(pts4d):
+      
+        if not good_pts4d[i]:
+            continue
+        pt = Point(mapp, p)
+        pt.add_observation(f1, i)
+        pt.add_observation(f2, i)
+ 
+    for pt1, pt2 in zip(f1.pts[idx1], f2.pts[idx2]):
+        u1, v1 = denormalize(K, pt1)
+        u2, v2 = denormalize(K, pt2)
+ 
+        cv2.circle(img, (u1,v1), 3, (0,255,0))
+        cv2.line(img, (u1,v1), (u2, v2), (255,0,0))
+ 
+    # 2-D display
+    display.paint(img)
+ 
+    # 3-D display
+    mapp.display()
+ 
+if __name__== "__main__":
+    cap = cv2.VideoCapture("./tenny-SLAM/Monocular/dashcamvid.mp4")
+ 
+    while cap.isOpened():
+        ret, frame = cap.read()
+        if ret == True:
+            process_frame(frame)
+        else:
+            break
